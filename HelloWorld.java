85480d2ddec98c48c602519f6fd20b761c112313
rajitreach <31933059+gititreach@users.noreply.github.com>
2020-03-30 15:25:25 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
  class Simple{  
     public static void main(String args[]){  
-     System.out.println("welcome to jenkins's world & rajesh");  
+     System.out.println("welcome to itreach");  
     }  
 }  
